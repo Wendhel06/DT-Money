104753327dbf67d999547325109a956a81875edf
Wendhel Alves Nogueira <wendhelalves7@gmail.com>
2023-10-07 13:33:33 -0300

    Creation and styling of transactions, header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyles } from "./styles/global";
+import { Transaction } from "./pages/Transactions";
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>Tudo funcionando ainda</h1>
+      <Transaction />
     </ThemeProvider>
   );
 }
