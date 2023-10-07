8f1b9061b1361b59127ed96c0dfab392b67a7500
Wendhel Alves Nogueira <wendhelalves7@gmail.com>
2023-10-07 12:34:40 -0300

    Initializing project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>Tudo funcionando ainda</h1>
+    </ThemeProvider>
+  );
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+
+::focus {
+  outline: 0px;
+  box-shadow: 0 0 0 2px ${(props => props.theme['green-500'])}
+}
+
+body {
+  background-color: ${(props) => props.theme['gray-800']};
+  color: ${(props) => props.theme['gray-100']};
+  webkit-font-smoothing: antialised;
+}
+
+body, input, textarea, button {
+  font: 400 1rem Roboto, sans-serif;
+}
+`
